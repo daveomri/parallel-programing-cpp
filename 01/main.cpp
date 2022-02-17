ddbe58c8aa61a968ea8167d03e0903f182bd4a3c
David Omrai <david.omrai@seznam.cz>
2022-02-17 12:23:20 +0100

    Added some filling for the main file, possibly will rename that, but it's fine for now

---
+++ 01/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author David Omrai (omraidav@fit.cvut.com)
+ * @brief First ni-pdp project sequence algorithm
+ * @version 0.1
+ * @date 2022-02-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[]) {
+    // todo
+    return 0;
+}
