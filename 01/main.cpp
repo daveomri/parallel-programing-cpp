ff05a78727bf32f4b277fc921016521a217ad8ae
David Omrai <david.omrai@seznam.cz>
2022-02-24 12:35:28 +0100

    Added few comments

--- 01/main.cpp
+++ 01/main.cpp
@@ -11,9 +11,28 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
+/**
+ * @brief Function loads the file with given name
+ *        in the graph_mbp folder and stores its
+ *        data to the array
+ * 
+ * @param fileName File name with graph prescription
+ */
+void loadGraph(string fileName) {
+    // todo
+}
+
+/**
+ * @brief Main funciton from witch the program runs
+ * 
+ * @param argc Number of input data
+ * @param argv Input data
+ * @return int 
+ */
 int main(int argc, char *argv[]) {
     // todo
     cout << "Hello world!";
