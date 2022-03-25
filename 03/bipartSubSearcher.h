ae7a87fef9cc46524ac78b5d088118840915a405
David Omrai <david.omrai@seznam.cz>
2022-03-25 20:14:32 +0100

    Tried to optimalize the code

---
+++ 03/bipartSubSearcher.h
@@ -0,0 +1,187 @@
+/**
+ * @file bipartSubSearcher.h
+ * @author David Omrai
+ * @brief All necessary functions for bipart. max subgraph search
+ * @version 0.1
+ * @date 2022-03-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <iomanip>
+#include <omp.h>
+
+#include "graph.h"
+
+void searchColorCombination(Graph* graph, Graph* subgraph, Results* results, int* cNodes, int trashWeights, int lastEdgeId, Edge* edge, int cN1, int cN2);
+
+/**
+ * @brief Function combinatons of edges 0-1, 1-0 or none
+ * 
+ * @param graph given graph to be used
+ * @param subgraph subgraph with result
+ * @param results all best results
+ * @param cNodes nodes colors
+ * @param trashWeights weight of edges that cannot be used
+ */
+void searchBiCoSubgraphs(Graph* graph, Graph* subgraph, Results* results, int* cNodes, int trashWeights, int lastEdgeId) {
+    // test if should continue
+    if (results->results != NULL) {
+        int freeWeights = graph->getWeightsSum() - trashWeights - subgraph->getWeightsSum();
+       
+        if ( (subgraph->getWeightsSum() + freeWeights) < results->results->weight ) {
+            delete subgraph;
+            delete[] cNodes;
+            return;
+        };
+    }
+
+    // get first available edge
+    Edge* edge = NULL;
+    int curEdgeId = 0;
+    //int a;
+    for (int i = lastEdgeId; i < graph->getEdgesNum(); i++) {
+        if (!subgraph->getEdgeWeight(graph->getEdges()[i]->n1, graph->getEdges()[i]->n2)) {
+            edge = graph->getEdges()[i];
+            curEdgeId = i;
+            break;
+        }
+    }
+
+    // no more available edge
+    if (edge == NULL) {
+        
+        // Is new solution better
+        if ((results->results == NULL && isConnected(subgraph) == true) || 
+            (results->results != NULL && results->results->weight <= subgraph->getWeightsSum() && isConnected(subgraph) == true)) {
+            #pragma omp critical 
+            {
+                addResult(results, subgraph, cNodes, graph->getEdgesNum());
+            }
+        };
+        
+        delete subgraph;
+        delete[] cNodes;
+        return;
+    }
+    
+
+    // if can be colored edge 0 1 and delete edge after you are done
+    if (
+        cNodes[edge->n1] != 1 && cNodes[edge->n2] != 0 &&
+        canColorNode(subgraph, cNodes, edge->n1, 0) &&
+        canColorNode(subgraph, cNodes, edge->n2, 1)
+        ) {
+            Graph* newSubgraph = subgraph->copyGraph();
+            int* newCNodes = copyVector(cNodes, graph->getNodesNum());
+            #pragma omp task
+            {
+                searchColorCombination(graph, newSubgraph, results, newCNodes, trashWeights, curEdgeId+1, edge, 0, 1);
+            }
+            //#pragma omp taskwait
+    }
+
+    // if can be colored edge 1 0 and delete edge after you are done
+    if (
+        cNodes[edge->n1] != 0 && cNodes[edge->n2] != 1 &&
+        canColorNode(subgraph, cNodes, edge->n1, 1) &&
+        canColorNode(subgraph, cNodes, edge->n2, 0)
+        ) {
+            Graph* newSubgraph = subgraph->copyGraph();
+            int* newCNodes = copyVector(cNodes, graph->getNodesNum());
+            #pragma omp task
+            {
+                searchColorCombination(graph, newSubgraph, results, newCNodes, trashWeights, curEdgeId+1, edge, 1, 0);
+            }
+            //#pragma omp taskwait
+    }
+
+    // dont use this edge
+    trashWeights += edge->weight;
+    Graph* newSubgraph = subgraph->copyGraph();
+    int* newCNodes = copyVector(cNodes, graph->getNodesNum());
+    #pragma omp task
+    {
+        searchBiCoSubgraphs(graph, newSubgraph, results, newCNodes, trashWeights, curEdgeId+1);
+    }
+    //#pragma omp taskwait
+    // // clean the mess
+    delete subgraph;
+    delete[] cNodes;
+}
+
+/**
+ * @brief todo
+ * 
+ * @param graph 
+ * @param subgraph 
+ * @param results 
+ * @param cNodes 
+ * @param trashWeights 
+ * @param lastEdgeId 
+ * @param cN1 
+ * @param cN2 
+ */
+void searchColorCombination(Graph* graph, Graph* subgraph, Results* results, int* cNodes, int trashWeights, int lastEdgeId, Edge* edge, int cN1, int cN2) {
+        // color the nodes
+        cNodes[edge->n1] = cN1;
+        cNodes[edge->n2] = cN2;
+
+        // add the edge to the graph
+        subgraph->addEdge(edge);
+
+        // continue the search with this setting
+        searchBiCoSubgraphs(graph, subgraph, results, cNodes, trashWeights, lastEdgeId);
+}
+
+Results* getMaxBiparSubgraph(Graph* graph) {
+
+    if (graph == NULL) return NULL;
+
+    // test if graph can be used
+    if (!isConnected(graph)) {
+        std::cout << "Graph is not connected" << '\n';
+        return NULL;
+    }
+  
+    // end if the graph itself is biparted
+    if (isBiparted(graph)) {
+        std::cout << "Graph is biparted" << '\n';
+    }
+
+    // create all neccesary components
+    Results* results = new Results;
+    results->results = NULL;
+
+    Graph* subgraph = new Graph(graph->getNodesNum());
+    graph->setEdges(createSorEdgesLL(graph));
+
+    
+    int* cNodes = new int[graph->getNodesNum()];
+    for (int i = 0; i < graph->getNodesNum(); i++) {
+        cNodes[i] = -1;
+    }
+
+    // color one node
+    cNodes[graph->getEdges()[0]->n1] = 0;
+
+    // parallel run
+    omp_set_num_threads(6);
+    #pragma omp parallel shared(graph, results)
+    {
+        // get the results
+        #pragma omp single
+        {
+            int ID = omp_get_thread_num();
+            std::cout << "NUM THREADS: " << ID << "\n";
+            searchBiCoSubgraphs(graph, subgraph, results, cNodes, 0, 0);
+        }
+    }
+    
+    // return the result
+    return results;
+}
---
+++ 03/main.cpp
@@ -0,0 +1,90 @@
+/**
+ * @file main.cpp
+ * @author David Omrai (omraidav@fit.cvut.cz)
+ * @brief First ni-pdp project sequence algorithm
+ * @version 0.1
+ * @date 2022-02-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <string>
+#include "bipartSubSearcher.h"
+
+using namespace std;
+
+/**
+ * @brief Main funciton from witch the program runs
+ * 
+ * @param argc Number of input data
+ * @param argv Input data
+ * @return int 
+ */
+int main(int argc, char *argv[]) {
+    // test if input is correct
+    if (argc != 2) {
+        cout << "Incorrect input, just name of graph is needed" << "\n";
+        return 1;
+    }
+
+    string graphName = argv[1];
+
+
+    Graph* graph = new Graph(graphName);
+
+    if (graph->getMatrix() == NULL) {
+        delete graph;
+        return 1;
+    }
+   
+
+    Results* results = getMaxBiparSubgraph(graph);
+
+    if (results->results != NULL) {
+        cout << "RESULTS" << endl;
+        
+        ResultNode* tmpRes = results->results;
+        while (tmpRes != NULL) {
+            cout << "Weight sum: " << endl;
+            cout << tmpRes->weight << endl;
+            cout << "Node colors:" << endl;
+            for (int i = 0; i < graph->getNodesNum(); i++) {
+                cout << i << ":" << tmpRes->cNodes[i] << ", ";
+            }
+            cout << '\n';
+
+            // cout << "Edges:" << endl;
+            // for (int i = 0; i < tmpRes->graph->getNodesNum(); i++) {
+            //     for (int j = 0; j < tmpRes->graph->getNodesNum(); j++) {
+            //         cout << " | " << setw(4) << tmpRes->graph->getEdgeWeight(i, j);
+            //     }
+            //     cout << " |" << endl;
+            // }
+
+            cout << "Edges:" << "\n";
+            int mi = 0;
+            for (int i = 0; i < graph->getNodesNum(); i++) {
+                for (int j = 0; j < graph->getNodesNum(); j++) {
+                    mi = graph->getMatrixPosition(i, j);
+                    if (mi == -1) cout << "  |   " << setw(4) << 0;
+                    else cout << "  |   " << setw(4) << tmpRes->graphWeights[mi];
+                    
+                }
+                cout << "  |" << "\n";
+            }
+
+            tmpRes = tmpRes->next;
+            delete results->results;
+            results->results = tmpRes;
+
+            cout << "-----------------------------------------------------------" << '\n';
+        }
+    }
+
+    delete results;
+    delete graph;
+
+    return 0;
+}
